605b69ac378b494f540f2f6a5243fd9a09297585
ktwjoshua05 <ktwjoshua05@gmail.com>
2025-12-17 13:36:24 +0900

    fix: vo file

--- src/main/java/org/example/wsd_final_team_project/vo/BirthdayPostVO.java
+++ src/main/java/org/example/wsd_final_team_project/vo/BirthdayPostVO.java
@@ -1,16 +1,20 @@
 package org.example.wsd_final_team_project.vo;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat; // 1. 이거 임포트 꼭!
 import java.util.Date;
 
 @Data
 public class BirthdayPostVO {
-    private int id;                  // id (PK)
-    private String groupName;        // group_name (소속)
-    private Date birthdayDate;       // birthday_date (생일)
-    private String birthdayImgUrl;   // birthday_img_url (이미지 경로)
-    private String birthdayPersonName; // birthday_person_name (생일자 이름)
-    private String celebrationText;  // celebration_text (내용)
-    private int viewCount;           // view_count (조회수)
-    private Date createdAt;          // created_at
+    private int id;
+    private String groupName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthdayDate;
+
+    private String birthdayImgUrl;
+    private String birthdayPersonName;
+    private String celebrationText;
+    private int viewCount;
+    private Date createdAt;
 }
